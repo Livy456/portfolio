d144689a77388f71ab79503e57c072d9facb7d15
Olivia <52472697+Livy456@users.noreply.github.com>
2024-02-28 12:05:19 -0500

    update global.js

--- global.js
+++ global.js
@@ -41,7 +41,8 @@ for (let p of pages)
    
     if (!ARE_WE_HOME && !url.startsWith("http"))
     {
-        url = "../portfolio/" + url 
+        url = "../" + url
+        // url = "../portfolio/" + url 
         //+ "index.html";
     }
 
